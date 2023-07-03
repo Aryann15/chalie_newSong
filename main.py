f2953f985a87742e6219243eee4afb3be1c25292
Aryann15 <rajlysm15@gmail.com>
2023-07-04 02:50:30 +0530

    filling up the two lists

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ text = open(filepath,'rb').read().decode(encoding='utf-8').lower()
 
 text = text[1000:30000]
 
-characters = sorted(set[text])
+characters = sorted(set(text))
 
 char_to_index = dict((c,i) for i,c in enumerate(characters))
 index_to_char = dict((i,c) for i,c in enumerate(characters))
@@ -20,4 +20,8 @@ SEQ_LENGTH =40
 STEP_SIZE =3
 
 sentences = []
-next_charcter = []
+next_character = []
+
+for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
+    sentences.append(text[i: i+SEQ_LENGTH])
+    next_character.append(text[i+SEQ_LENGTH])

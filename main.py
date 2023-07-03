8f2c03b66fe0ceed641e0c563aa951f3e25215dd
Aryann15 <rajlysm15@gmail.com>
2023-07-04 02:44:24 +0530

    set off text

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ filepath = 'charlie.txt'
 
 text = open(filepath,'rb').read().decode(encoding='utf-8').lower()
 
+text = text[1000:30000]
 
+characters = sorted(set[text])
+
+char_to_index = dict((c,i) for i,c in enumerate(characters))
+index_to_char = dict((i,c) for i,c in enumerate(characters))
